923d6e3b49133d3fab0717d2c52cef26affd52db
raymondtri <ray@raytri.com>
2025-02-01 14:35:32 -0500

    phew

--- packages/matchmaker/src/index.ts
+++ packages/matchmaker/src/index.ts
@@ -18,36 +18,66 @@ export class Queue {
 
   private _driver: MatchMakerDriver;
 
-  public readonly queueCreate: boolean = false;
   public readonly targetRoomsPerProcess: number = 10;
   public readonly processFilterConditions: { [field: string]: any } = {};
+  public readonly roomMaxClientMap: { [roomName: string]: number } = {};
 
   constructor(driver: MatchMakerDriver,  options?: any){
     this._driver = driver;
 
     if(this._driver.externalMatchmaker) throw new Error('External Matchmaking must be set to false on the matchmaker processor, it IS the external matchmaker.')
 
-    if(options?.queueCreate) this.queueCreate = true;
     if(options?.targetRoomsPerProcess) this.targetRoomsPerProcess = options.targetRoomsPerProcess;
-    if(options?.processFilterConditions) this.processFilterConditions = options.processFilterConditions
+    if(options?.processFilterConditions) this.processFilterConditions = options.processFilterConditions;
+    if(options?.roomMaxClientMap) this.roomMaxClientMap = options.roomMaxClientMap;
   }
+
+  // so this process function in all reality likely should be the onus of the game creator to operate
+  // in fact I think a better pattern would be to publish this in a template after cleaning up
+  // since to handle concurrent joinOrCreate you have to "synthetically" create a room
+  // and that process of synthetically creating a room can result in nearly any outcome
+  // so just having a default here that relies on max clients seems far better
+  // maybe we pass in the static MyRoom objects and pull max clients off of that etc.
+  // but yeah for concurrent matchmaking, I think you should have to write your own process and pass it in here
+  // or just handle this yourself
 
   async process(){
     const startTime = hrtime.bigint();
-    // first we need to get all of the rooms that are eligible for matchmaking
-    const eligibleRooms = await this._driver.query({eligibleForMatchmaking: true});
     const endTime = hrtime.bigint();
-
     const durationInMilliseconds = Number(endTime - startTime) / 1_000_000;
-    console.log(`Found and deserialized ${eligibleRooms.length} eligible rooms in ${durationInMilliseconds} milliseconds.`)
 
-    // console.log(eligibleRooms)
-    // so now we have all of the rooms that are eligible for matchmaking
+    // console.log(`Found and deserialized ${eligibleRooms.length} eligible rooms in ${durationInMilliseconds} milliseconds.`)
 
     // let's get the requests
     const requests = await this._driver.spliceMatchmakingRequests();
     console.log(requests)
-    // and now we have all of the requests that need to be made
+
+    // now we need to get all of the rooms that are eligible for matchmaking
+    const eligibleRooms = await this._driver.query({eligibleForMatchmaking: true});
+    console.log(eligibleRooms)
+    // so now we have all of the rooms that are eligible for matchmaking
+
+    const roomMap: { [roomId: string]: any } = {};
+    eligibleRooms.forEach((room:any) => {
+      roomMap[room.roomId] = room;
+    })
+
+    const findRoominEligibleRooms = (conditions: any) => {
+      return Object.entries(roomMap).filter(([roomId, room]) => {
+        if (!room) return false;
+        if (room.clients >= room.maxClients) return false;
+
+        for (const field in conditions) {
+          if (
+            conditions.hasOwnProperty(field) &&
+            room[field] !== conditions[field]
+          ) {
+            return false;
+          }
+        }
+        return true;
+      }).map(([roomId, room]) => room);
+    }
 
     // and now we need to get the processes ranked by eligibility
     const processes = await this._driver.queryProcesses(this.processFilterConditions,requests.length);
@@ -61,7 +91,8 @@ export class Queue {
 
     // how do we compare other process information? I really don't know, but for now here's the scors
     const findProcessinEligibleProcesses = (conditions: any) => {
-      return processes.filter((process:any) => {
+      return Object.entries(processMap).filter(([processId, process]) => {
+        if (!process) return false;
         for (const field in conditions) {
           if (
             conditions.hasOwnProperty(field) &&
@@ -71,9 +102,9 @@ export class Queue {
           }
         }
         return true;
-      }).sort((a:any, b:any) => {
-        return a.score - b.score;
-      })
+      }).sort((a, b) => {
+        return a[1].score - b[1].score;
+      }).map(([processId, process]) => process);
     }
 
     // TODO matchmake the requests
@@ -82,103 +113,107 @@ export class Queue {
     // however we now need a second score here so that processes are assessed based on
     // the actual room requests that are coming in
 
-    const creates: requestResponse[] = [];
+    const promises: Promise<any>[] = [];
 
-    /*
-    const findRoomInEligibleRooms = (roomName:string, conditions: any) => {
-      const oldRooms = eligibleRooms.filter((room:any) => {
-        if(room.roomName !== roomName) return false;
-
-        for (const field in conditions) {
-          if (
-            conditions.hasOwnProperty(field) &&
-            room[field] !== conditions[field]
-          ) {
-            return false;
-          }
-        }
-        return true;
-      })
-
-      const newRooms = creates.filter((room:any) => {
-        for (const field in conditions) {
-          if (
-            conditions.hasOwnProperty(field) &&
-          )
-        }
-      })
-    }
-    */
-
-    // I think we have to loop the requests multiple times which is gross but whatever
-    // this is more proof of concept than production ready
-    // 1. match up any creates with any processes that are eligible
     requests.forEach((request:any) => {
-      if(request.method === 'create'){ // we just automatically handle these
-        const process = findProcessinEligibleProcesses({})[0];
-        const url = new URL('http://' + process.publicAddress);
+      if(request.method === 'joinOrCreate'){
+        // first we try to find an existing room that matches the criteria
+        const room = findRoominEligibleRooms({name: request.roomName, ...request.clientOptions})[0];
 
-        const match = {
-          method: 'create',
-          roomName: request.roomName,
-          options: request.clientOptions,
-          settings: {
-            hostname: url.hostname,
-            secure: false, // TODO handle how we determine security
-            pathname: url.pathname,
-            port: url.port ? parseInt(url.port) : undefined
+        if(room){
+          const url = new URL('http://' + room.publicAddress);
+
+          const match = {
+            method: 'join',
+            roomName: request.roomName,
+            options: request.clientOptions,
+            settings: {
+              hostname: url.hostname,
+              secure: false, // TODO handle how we determine security
+              pathname: url.pathname,
+              port: url.port ? parseInt(url.port) : undefined
+            }
           }
+
+          // increment the room client count
+          roomMap[room.roomId].clients += 1;
+
+          // and then we dispatch it
+          const pubd = this._driver.client.publish(`matchmaking:matches:${request.requestId}`, JSON.stringify(match));
+          promises.push(pubd)
+        } else {
+          const process = findProcessinEligibleProcesses({})[0];
+          const url = new URL('http://' + process.publicAddress);
+
+          const match = {
+            method: 'create',
+            roomName: request.roomName,
+            options: request.clientOptions,
+            settings: {
+              hostname: url.hostname,
+              secure: false, // TODO handle how we determine security
+              pathname: url.pathname,
+              port: url.port ? parseInt(url.port) : undefined
+            }
+          }
+
+          roomMap[nanoid(9)] = {
+            clients: 1,
+            maxClients: this.roomMaxClientMap[request.roomName] || 4,
+            name: request.roomName,
+            publicAddress: process.publicAddress,
+            processId: process.processId,
+            eligibleForMatchmaking: true,
+          }
+          processMap[process.processId].score += 1;
+
+          // and then we dispatch it
+          const pubd = this._driver.client.publish(`matchmaking:matches:${request.requestId}`, JSON.stringify(match));
+          promises.push(pubd);
         }
+      } else if (request.method === 'join') {
+        const room = findRoominEligibleRooms({name: request.roomName, ...request.clientOptions})[0];
 
-        creates.push(match);
+        if(room){
+          const url = new URL('http://' + room.publicAddress);
 
-        // increment the score here? again, I really don't know, this all feels so custom based on the individual game use case
-        // I really think this whole process piece gets exposed for people to define themself but it's just a part of the template
-        processMap[process.processId].score += 1;
+          const match = {
+            method: 'join',
+            roomName: request.roomName,
+            options: request.clientOptions,
+            settings: {
+              hostname: url.hostname,
+              secure: false, // TODO handle how we determine security
+              pathname: url.pathname,
+              port: url.port ? parseInt(url.port) : undefined
+            }
+          }
 
-        // and then we dispatch it
-        this._driver.client.publish(`matchmaking:matches:${request.requestId}`, JSON.stringify(match));
+          // increment the room client count
+          roomMap[room.roomId].clients += 1;
+
+          // and the processid client count
+          processMap[room.processId].score += 1;
+
+          // and then we dispatch it
+          const pubd = this._driver.client.publish(`matchmaking:matches:${request.requestId}`, JSON.stringify(match));
+          promises.push(pubd);
+        }
       }
     })
 
-    // 2. now we match up any joinOrCreates
+    await Promise.all(promises);
 
+    // phew
 
-    // 2. match up any joins with any rooms that already match the criteria
-    // 3. handle the joinOrCreates, since we can basically transmute them into a join or a create
-
-    // instead of touching the end server directly, because then this could need to handle tens of thousands of open http connections
-    // we should return a response to the client that they should reconnect to the server that is handling their room
-    // and then we need to correct the joinOrCreate to be a join or a create based on the logic ran above
-
-
-    /*
-    const response = {
-      method: 'create',
-      roomName: 'test',
-      options: {},
-      settings: { // you must return settings so the client can recalibrate
-        hostname: 'localhost',
-        secure: false,
-        pathname: undefined,
-        port: undefined
-      }
-    }
-
-    if(response.method === 'joinOrCreate'){
-      throw new Error("You cannot return a joinOrCreate response from the matchmaker processor. You must return a join or a create.")
-    }
-    */
-
-    // this._driver.client.publish(`matchmaking:matches:${requestId}`, JSON.stringify(response));
   }
 
   get queueableMethods(){
-    return ['joinOrCreate', 'join'].concat(this.queueCreate ? ['create'] : []);
+    return ['joinOrCreate', 'join']
   }
 
-  get dispatchableMethods(){
-    return ['joinById', 'reconnect'].concat(this.queueCreate ? [] : ['create']);
+  get dispatchableMethods(){ // create should NEVER be queueable because then we have lots of weird room data issues
+    return ['joinById', 'reconnect', 'create'];
   }
 
   // FIRST BOTTLENECK - number of connected clients to the redis instance. If 10,000 by default then this will only handle 9999 concurrent matchmaking requests
